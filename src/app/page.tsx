605b42ae0a8b259751ce2f854e3eab5c9f896c8a
Krzysztof Kaptur <krzysztof.kaptur@gmail.com>
2024-08-10 09:28:55 +0200

    feat(landing-page): Add link to dashboard if user logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
+import { createClient } from '@/lib/utils/supabase/server'
 import Link from 'next/link'
 
 import { Text } from '@/components'
 
 export default async function Home() {
+  const supabase = createClient()
+
+  const { error } = await supabase.auth.getUser()
+
   return (
     <main className='flex min-h-screen flex-col justify-center items-center gap-4'>
       <Text variant='h1'>This is my project</Text>
-      <Link href='/auth/login'>Login</Link>
+      {error ? (
+        <Link href='/auth/login'>Login</Link>
+      ) : (
+        <Link href='/dashboard'>Dashboard</Link>
+      )}
     </main>
   )
 }
